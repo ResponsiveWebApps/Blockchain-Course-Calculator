a196bd3488bf8b21c4aebec831d724ecca0aa902
ResponsiveWebApps <donrochemedia@gmail.com>
2017-11-29 21:35:21 +0100

    Update CalculatorV2.sol
    Made public functions to fix errors.

--- contracts/CalculatorV2.sol
+++ contracts/CalculatorV2.sol
@@ -2,46 +2,46 @@ pragma solidity ^0.4.6;
 
 contract CalculatorV2 {
 
-  uint result;
+  uint public result;
 
   event NumberAdded(uint n);
   event NumberSubtracted(uint n);
   event NumberMultiplied(uint n);
   event NumberDivided(uint n);
 
-  function CalculatorV2(uint num) {
+  function CalculatorV2(uint num) public {
     // constructor
     result=num;
   }
 
   // returns the result
-  function getResult() constant returns (uint){
+  function getResult() public constant returns (uint) {
     return result;
   }
 
   // result = result + num
-  function addToNumber(uint num) returns (uint) {
+  function addToNumber(uint num) public returns (uint) {
     result += num;
     NumberAdded(num);
     return result;
   }
 
   // result = result - num
-  function substractNumber(uint num) returns (uint) {
+  function substractNumber(uint num) public returns (uint) {
     result -= num;
     NumberSubtracted(num);
     return result;
   }
 
   // result = result * num
-  function multiplyWithNumber(uint num) returns (uint) {
+  function multiplyWithNumber(uint num) public returns (uint) {
     result *= num;
     NumberMultiplied(num);
     return result;
   }
 
   // result = result / num
-  function divideByNumber(uint num) returns (uint) {
+  function divideByNumber(uint num) public returns (uint) {
     result /= num;
     NumberDivided(num);
     return result;
